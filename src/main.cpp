1af7bdcce705a75f9edc0a3ef064168e16e76005
Hanyuan Li <work@hanyuone.live>
2025-04-01 15:35:30 +1100

    feat: very basic linear sweep

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,83 @@
-#include <Zydis/Zydis.h>
 #include <inttypes.h>
-#include <stdio.h>
 
-int main() {
-  ZyanU8 data[] = {0x51, 0x8D, 0x45, 0xFF, 0x50, 0xFF, 0x75, 0x0C, 0xFF,
-                   0x75, 0x08, 0xFF, 0x15, 0xA0, 0xA5, 0x48, 0x76, 0x85,
-                   0xC0, 0x0F, 0x88, 0xFC, 0xDA, 0x02, 0x00};
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <vector>
 
-  // The runtime address (instruction pointer) was chosen arbitrarily here in
-  // order to better visualize relative addressing. In your actual program, set
-  // this to e.g. the memory address that the code being disassembled was read
-  // from.
-  ZyanU64 runtime_address = 0x007FFFFFFF400000;
+#include <Zydis/Zydis.h>
 
-  // Loop over the instructions in our buffer.
-  ZyanUSize offset = 0;
-  ZydisDisassembledInstruction instruction;
-  while (ZYAN_SUCCESS(ZydisDisassembleIntel(
-      /* machine_mode:    */ ZYDIS_MACHINE_MODE_LONG_64,
-      /* runtime_address: */ runtime_address,
-      /* buffer:          */ data + offset,
-      /* length:          */ sizeof(data) - offset,
-      /* instruction:     */ &instruction))) {
-    printf("%016" PRIX64 "  %s\n", runtime_address, instruction.text);
+std::vector<ZyanU8> get_zydis_bytes(std::string fname) {
+  std::ifstream input(fname);
+
+  std::vector<char> bytes((std::istreambuf_iterator<char>(input)),
+                          (std::istreambuf_iterator<char>()));
+  int length = bytes.size();
+
+  std::vector<ZyanU8> zydis_bytes(length);
+
+  for (int i = 0; i < length; i++) {
+    zydis_bytes[i] = (ZyanU8)bytes[i];
+  }
+
+  return zydis_bytes;
+}
+
+std::vector<ZydisDisassembledInstruction> get_zydis_insts(ZyanU8 *buffer) {
+  std::vector<ZydisDisassembledInstruction> instructions;
+  ZyanU64 runtime_address = 0x1000;
+  ZyanUSize offset = 0x1000;
+
+  while (true) {
+    ZydisDisassembledInstruction instruction;
+    ZyanUSize length = std::min(sizeof(buffer) - offset,
+                                (ZyanUSize)ZYDIS_MAX_INSTRUCTION_LENGTH);
+    auto status =
+        ZydisDisassembleIntel(ZYDIS_MACHINE_MODE_LONG_64, runtime_address,
+                              buffer + offset, length, &instruction);
+
+    if (offset > 0x1144 || ZYAN_FAILED(status)) {
+      break;
+    }
+
+    instructions.push_back(instruction);
+
     offset += instruction.info.length;
     runtime_address += instruction.info.length;
   }
 
-  return 0;
+  return instructions;
+}
+
+int main() {
+  std::string fname = "../bin/exe/empty";
+
+  std::vector<ZyanU8> zydis_bytes = get_zydis_bytes(fname);
+  ZyanU8 *zydis_buffer = new ZyanU8[zydis_bytes.size()];
+  std::copy(zydis_bytes.begin(), zydis_bytes.end(), zydis_buffer);
+
+  std::vector<ZydisDisassembledInstruction> instructions =
+      get_zydis_insts(zydis_buffer);
+
+  for (auto inst : instructions) {
+    ZydisEncoderRequest enc_req;
+    ZydisEncoderDecodedInstructionToEncoderRequest(
+        &inst.info, inst.operands, inst.info.operand_count_visible, &enc_req);
+
+    uint8_t new_bytes[ZYDIS_MAX_INSTRUCTION_LENGTH];
+    ZyanUSize new_instr_length = sizeof(new_bytes);
+    ZydisEncoderEncodeInstruction(&enc_req, new_bytes, &new_instr_length);
+
+    std::cout << std::hex << inst.runtime_address << "\t";
+
+    for (int i = 0; i < new_instr_length; i++) {
+      std::cout << std::setfill('0') << std::setw(2) << std::uppercase
+                << std::hex << (0xFF & new_bytes[i]) << " ";
+    }
+
+    std::cout << "\t\t";
+
+    std::cout << inst.text << std::endl;
+  }
 }
