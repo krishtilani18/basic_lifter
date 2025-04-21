8428528b5c9a23a6f00a5d0dd7125540c3e3e2f0
Hanyuan Li <work@hanyuone.live>
2025-04-21 12:56:02 +1000

    chore: split Disassembler file

--- include/disass/Disassembler.hpp
+++ include/disass/Disassembler.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <llvm/IR/LLVMContext.h>
-#include <remill/Arch/Arch.h>
 #include <remill/Arch/Instruction.h>
 
 #include <elf.hpp>
@@ -11,12 +9,4 @@
 class Disassembler {
 public:
   virtual std::vector<remill::Instruction> Disassemble(X86Function) = 0;
-};
-
-class NaiveDisassembler : Disassembler {
-public:
-  NaiveDisassembler(const remill::Arch *arch): arch(arch) {}
-  std::vector<remill::Instruction> Disassemble(X86Function) override;
-private:
-  const remill::Arch *arch;
 };
