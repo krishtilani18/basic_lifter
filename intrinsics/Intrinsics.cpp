3f9c57f6d9718d7d0ec90c368dec8760be902a11
Hanyuan Li <work@hanyuone.live>
2025-08-02 23:11:52 +1000

    style: move extern C to block

--- intrinsics/Intrinsics.cpp
+++ intrinsics/Intrinsics.cpp
@@ -7,14 +7,15 @@ typedef uint64_t addr_t;
 struct State;
 using Memory = std::map<uint64_t, uint8_t>;
 
-extern "C" Memory *__lifter_init_memory() {
+extern "C" {
+Memory *__lifter_init_memory() {
     Memory *memory = new Memory;
     return memory;
 }
 
-extern "C" void __lifter_free_memory(Memory *memory) { delete memory; }
+void __lifter_free_memory(Memory *memory) { delete memory; }
 
-extern "C" uint32_t __remill_read_memory_32(Memory *memory, addr_t addr) {
+uint32_t __remill_read_memory_32(Memory *memory, addr_t addr) {
     uint32_t result = 0;
 
     for (size_t offset = 0; offset < 4; offset++) {
@@ -34,7 +35,7 @@ extern "C" uint32_t __remill_read_memory_32(Memory *memory, addr_t addr) {
     return result;
 }
 
-extern "C" uint64_t __remill_read_memory_64(Memory *memory, addr_t addr) {
+uint64_t __remill_read_memory_64(Memory *memory, addr_t addr) {
     uint64_t result = 0;
 
     for (size_t offset = 0; offset < 8; offset++) {
@@ -54,8 +55,7 @@ extern "C" uint64_t __remill_read_memory_64(Memory *memory, addr_t addr) {
     return result;
 }
 
-extern "C" Memory *__remill_write_memory_32(Memory *memory, addr_t addr,
-                                            uint32_t val) {
+Memory *__remill_write_memory_32(Memory *memory, addr_t addr, uint32_t val) {
     for (size_t offset = 0; offset < 4; offset++) {
         size_t shift = (3 - offset) * 8;
         uint32_t shifted = val >> shift;
@@ -66,8 +66,7 @@ extern "C" Memory *__remill_write_memory_32(Memory *memory, addr_t addr,
     return memory;
 }
 
-extern "C" Memory *__remill_write_memory_64(Memory *memory, addr_t addr,
-                                            uint64_t val) {
+Memory *__remill_write_memory_64(Memory *memory, addr_t addr, uint64_t val) {
     for (size_t offset = 0; offset < 8; offset++) {
         size_t shift = (7 - offset) * 8;
         uint64_t shifted = val >> shift;
@@ -78,44 +77,35 @@ extern "C" Memory *__remill_write_memory_64(Memory *memory, addr_t addr,
     return memory;
 }
 
-extern "C" bool __remill_flag_computation_sign(bool result, ...) {
-    return result;
-}
+bool __remill_flag_computation_sign(bool result, ...) { return result; }
 
-extern "C" bool __remill_flag_computation_zero(bool result, ...) {
-    return result;
-}
+bool __remill_flag_computation_zero(bool result, ...) { return result; }
 
-extern "C" bool __remill_flag_computation_carry(bool result, ...) {
-    return result;
-}
+bool __remill_flag_computation_carry(bool result, ...) { return result; }
 
-extern "C" bool __remill_flag_computation_overflow(bool result, ...) {
-    return result;
-}
+bool __remill_flag_computation_overflow(bool result, ...) { return result; }
 
-extern "C" bool __remill_compare_neq(bool result) { return result; }
+bool __remill_compare_neq(bool result) { return result; }
 
-extern "C" bool __remill_compare_uge(bool result) { return result; }
+bool __remill_compare_uge(bool result) { return result; }
 
-extern "C" bool __remill_compare_sle(bool result) { return result; }
+bool __remill_compare_sle(bool result) { return result; }
 
-extern "C" bool __remill_compare_sgt(bool result) { return result; }
+bool __remill_compare_sgt(bool result) { return result; }
 
-extern "C" bool __remill_compare_eq(bool result) { return result; }
+bool __remill_compare_eq(bool result) { return result; }
 
-extern "C" uint8_t __remill_undefined_8(void) { return 0; }
+uint8_t __remill_undefined_8(void) { return 0; }
 
-extern "C" Memory *__remill_error(State &state, addr_t addr, Memory *memory) {
+Memory *__remill_error(State &state, addr_t addr, Memory *memory) {
     return memory;
 }
 
-extern "C" Memory *__remill_function_call(State &state, addr_t addr,
-                                          Memory *memory) {
+Memory *__remill_function_call(State &state, addr_t addr, Memory *memory) {
     return memory;
 }
 
-extern "C" Memory *__remill_function_return(State &state, addr_t addr,
-                                            Memory *memory) {
+Memory *__remill_function_return(State &state, addr_t addr, Memory *memory) {
     return memory;
+}
 }
