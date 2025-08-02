2fca435c060b7009fda5f946dfd4e0be7fedeefd
Hanyuan Li <work@hanyuone.live>
2025-08-02 17:21:26 +1000

    feat: change example suite to not dynamically link to glibc

---
+++ example/c/square.c
@@ -0,0 +1,5 @@
+int main() {
+    int sq_3 = 3 * 3;
+    int sq_4 = 4 * 4;
+    return sq_3 + sq_4;
+}
---
+++ example/ll/square.s
@@ -0,0 +1,27 @@
+	.text
+	.file	"square.c"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	movl	$0, -12(%rbp)
+	movl	$9, -8(%rbp)
+	movl	$16, -4(%rbp)
+	movl	-8(%rbp), %eax
+	addl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa %rsp, 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.ident	"Ubuntu clang version 18.1.3 (1ubuntu1)"
+	.section	".note.GNU-stack","",@progbits
