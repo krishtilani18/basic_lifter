86c034130456079b7a1c0aa6bacecf4318c0a26c
Hanyuan Li <work@hanyuone.live>
2025-10-13 16:42:28 +1100

    feat: move intrinsics/external files to util folder

--- util/External.cpp
+++ util/External.cpp
@@ -1,15 +1,19 @@
 #include <cstdio>
 #include <map>
 
-#include "remill/Arch/X86/Runtime/State.h"
+#include <remill/Arch/X86/Runtime/State.h>
+#include <util/Memory.hpp>
 
-Memory *EXTERNAL_printf(State *state, addr_t program_counter, Memory *memory) {
+extern "C" {
+LifterMemory *EXTERNAL_printf(State *state, addr_t program_counter,
+                              LifterMemory *memory) {
     // 2nd argument stored in %esi
     int value = state->gpr.rsi.dword;
 
     // Currently, `.rodata` is not integrated, so use fixed value
     // to demonstrate functionality
     printf("%d\n", value);
-    
+
     return memory;
+}
 }
